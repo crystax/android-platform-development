e7869f441fefaac978df67e4d62a8c065bd9245d
Dmitry Moskalchuk <dm@crystax.net>
2011-07-23 19:00:51 +0400

    Fix size_t definition for x86

--- ndk/platforms/android-9/arch-x86/include/machine/_types.h
+++ ndk/platforms/android-9/arch-x86/include/machine/_types.h
@@ -38,11 +38,15 @@
 /* the kernel defines size_t as unsigned int, but g++ wants it to be unsigned long */
 #ifndef _SIZE_T_DEFINED_
 #  define _SIZE_T_DEFINED_
+/*
 #  ifdef ANDROID
+*/
      typedef unsigned int  size_t;
+/*
 #  else
      typedef unsigned long  size_t;
 #  endif
+*/
 #endif
 #if !defined(_SSIZE_T) && !defined(_SSIZE_T_DEFINED_)
 #define _SSIZE_T
