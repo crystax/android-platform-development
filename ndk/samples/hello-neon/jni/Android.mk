d7e23043a49fefab2d258af0f89ca5150b9ba722
Dmitry Moskalchuk <dm@crystax.net>
2014-12-28 01:47:28 +0300

    [hello-neon] Disable building of NEON intrinsics for clang/x86
    See https://tracker.crystax.net/issues/824 for details.

--- ndk/samples/hello-neon/jni/Android.mk
+++ ndk/samples/hello-neon/jni/Android.mk
@@ -7,11 +7,14 @@ LOCAL_MODULE := helloneon
 LOCAL_SRC_FILES := helloneon.c
 
 ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi-v7a x86))
+# Clang toolchain don't support yet NEON for x86
+ifeq (,$(and $(filter clang%,$(NDK_TOOLCHAIN_VERSION)),$(filter x86,$(TARGET_ARCH_ABI))))
     LOCAL_CFLAGS := -DHAVE_NEON=1
 ifeq ($(TARGET_ARCH_ABI),x86)
     LOCAL_CFLAGS += -mssse3
 endif
     LOCAL_SRC_FILES += helloneon-intrinsics.c.neon
+endif
 endif
 
 LOCAL_STATIC_LIBRARIES := cpufeatures
