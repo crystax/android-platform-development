bebffbcdd87ea55054cbc6ec18b0262f1e65216f
Alexander Zhukov <zuav@crystax.net>
2015-06-04 13:13:39 +0300

    clang does not support neon for x86_64

--- ndk/samples/hello-neon/jni/Android.mk
+++ ndk/samples/hello-neon/jni/Android.mk
@@ -6,18 +6,16 @@ LOCAL_MODULE := helloneon
 
 LOCAL_SRC_FILES := helloneon.c
 
-# todo: zuav:
-#ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi-v7a x86))
-# Clang toolchain don't support yet NEON for x86
-#ifeq (,$(and $(filter clang%,$(NDK_TOOLCHAIN_VERSION)),$(filter x86,$(TARGET_ARCH_ABI))))
 ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi-v7a x86 x86_64 arm64-v8a))
+# Clang toolchain don't support yet NEON for x86
+ifeq (,$(and $(filter clang%,$(NDK_TOOLCHAIN_VERSION)),$(filter x86_64,$(TARGET_ARCH_ABI))))
     LOCAL_CFLAGS := -DHAVE_NEON=1
 ifeq ($(TARGET_ARCH_ABI),x86)
     LOCAL_CFLAGS += -mssse3
 endif
     LOCAL_SRC_FILES += helloneon-intrinsics.c.neon
 endif
-#endif
+endif
 
 LOCAL_STATIC_LIBRARIES := cpufeatures
 
